fb9f5f34ab2107b0155550c2347660aa4f5d0356
Owusu-Ansah Solomon <owusuansahsolomon39@gmail.com>
2022-12-15 15:50:46 +0000

    fixed navigation bug

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/youtube");
+  }, []);
+
   return <HomeContainer>Home</HomeContainer>;
 };
 
